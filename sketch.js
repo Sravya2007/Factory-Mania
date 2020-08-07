89202c1e222342b135c04debff6397469fb1fb43
Sravya2007 <sravyamarepally@outlook.com>
2020-08-07 22:17:08 +0530

    Player functionality added!

--- sketch.js
+++ sketch.js
@@ -7,6 +7,9 @@ var bin1, bin2, bin3, bin4, bin5;
 var binImg1, binImg2, binImg3, binImg4, binImg5;
 var bio, glass, metal, paper, plastic;
 var bioImg, glassImg, metalImg, paperImg, plasticImg;
+var rand, rand1, waste;
+var openlid1, openlid2, openlid3, openlid4, openlid5;
+var openlid1Img, openlid2Img, openlid3Img, openlid4Img, openlid5Img;
 
 function preload() {
   prodImg = loadImage("Images/Producing Machine.png");
@@ -24,10 +27,20 @@ function preload() {
   metalImg = loadImage("Images/Metal Waste.png");
   paperImg = loadImage("Images/Paper Waste.png");
   plasticImg = loadImage("Images/Plastic Waste.png");
+
+  openlid1Img = loadImage("Images/Bio Open Lid.png");
+  openlid2Img = loadImage("Images/Glass Open Lid.png");
+  openlid3Img = loadImage("Images/Metal Open Lid.png");
+  openlid4Img = loadImage("Images/Paper Open Lid.png");
+  openlid5Img = loadImage("Images/Plastic Open Lid.png");
+
 }
 
 function setup() {
-  createCanvas(1800,800);
+  createCanvas(1800, 800);
+  
+  waste = createSprite(500, 100);
+  waste.visible = false;
 
   prodMachine = createSprite(395, 400, 200, 750);
   prodMachine.addImage(prodImg);
@@ -92,10 +105,113 @@ function setup() {
   bin5 = createSprite(1430, 700);
   bin5.addImage("binImg", binImg5);
   bin5.scale = 0.8;
+
+  openlid1 = createSprite(1430, 80);
+  openlid1.visible = false;
+  openlid1.addImage("openLid", openlid1Img);
+
+  openlid2 = createSprite(1430, 230);
+  openlid2.visible = false;
+  openlid2.addImage("openLid", openlid2Img);
+
+  openlid3 = createSprite(1430, 380);
+  openlid3.visible = false;
+  openlid3.addImage("openLid", openlid3Img);
+
+  openlid4 = createSprite(1430, 530);
+  openlid4.visible = false;
+  openlid4.addImage("openLid", openlid4Img);
+
+  openlid5 = createSprite(1430, 680);
+  openlid5.visible = false;
+  openlid5.addImage("openLid", openlid5Img);
 }
 
 function draw() {
   background("Black");
 
+  if(frameCount % 100 === 0) {
+    waste = createSprite(500, 100);
+    waste.scale = 0.8;
+    waste.life = 284;
+    waste.velocityX = 3;
+
+    rand = Math.round(random(1, 5));
+    switch(rand) {
+        case 1:
+        waste.y = 100;
+        break;
+  
+        case 2:
+        waste.y = 250;
+        break;
+  
+        case 3:
+        waste.y = 400;
+        break;
+  
+        case 4:
+        waste.y = 550;
+        break;
+  
+        case 5:
+        waste.y = 700;
+        break;
+  
+        default:
+          break;
+    }
+  
+    rand1 = Math.round(random(1, 5));
+    switch(rand1) {
+      case 1:
+      waste.addImage("waste", bioImg);
+      break;
+
+      case 2:
+      waste.addImage("waste1", glassImg);
+      break;  
+
+      case 3:
+      waste.addImage("waste2", metalImg);
+      break;
+
+      case 4:
+      waste.addImage("waste3", paperImg);
+      break;
+
+      case 5:
+      waste.addImage("waste4", plasticImg);
+      break;
+      
+      default:
+      break;
+    }
+
+  } 
+  
+  if(frameCount % 100 < 100 && frameCount % 100 > 5) {
+    openlid1.visible = true;
+    openlid2.visible = true;
+    openlid3.visible = true;
+    openlid4.visible = true;
+    openlid5.visible = true;
+  } else {
+    openlid1.visible = false;
+    openlid2.visible = false;
+    openlid3.visible = false;
+    openlid4.visible = false;
+    openlid5.visible = false;
+  }
+
   drawSprites();
+}
+
+function mouseDragged() {
+  if((waste.x <= 1430 && waste.x >= 500) && (waste.y > 0 && waste.y < 800)) {
+    waste.x = mouseX;
+    waste.y = mouseY;
+  } else {
+    waste.visible = false;
+  }
 }
