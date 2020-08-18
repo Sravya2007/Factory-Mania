197980e9d97ef0d01621e9f8537bc482b4bd506a
Sravya2007 <sravyamarepally@outlook.com>
2020-08-18 09:38:51 +0530

    Change in width and height

--- sketch.js
+++ sketch.js
@@ -65,11 +65,11 @@ function setup() {
 
   //creating all the sprites and assigning the values and properties
   
-  waste = createSprite(500, 100);
+  waste = createSprite(width - 1420, height - 869);
   waste.addAnimation("bioImg", bioImg);
   waste.visible = false;
 
-  prodMachine = createSprite(150, 400, 200, 750);
+  prodMachine = createSprite(width - 1770, height - 569, 200, 750);
   prodMachine.addImage(prodImg);
   prodMachine.scale = 1;
   angleMode(DEGREES);
@@ -77,76 +77,76 @@ function setup() {
 
   points = 0;
 
-  belt1 = createSprite(1350, 100);
+  belt1 = createSprite(width - 570, height - 869);
   belt1.addImage("beltImg", beltImg);
   belt1.scale = 1.5;
 
-  belt2 = createSprite(1350, 250);
+  belt2 = createSprite(width - 570, height - 719);
   belt2.addImage("beltImg", beltImg);
   belt2.scale = 1.5;
 
-  belt3 = createSprite(600, 400);
+  belt3 = createSprite(width - 1320, height - 569);
   belt3.addImage("beltImg", beltImg);
   belt3.scale = 1.5;
 
-  beltExt3 = createSprite(1350, 400);
+  beltExt3 = createSprite(width - 570, height - 569);
   beltExt3.addImage("beltImg", beltImg);
   beltExt3.scale = 1.5;
 
-  belt4 = createSprite(1350, 550);
+  belt4 = createSprite(width - 570, height - 419);
   belt4.addImage("beltImg", beltImg);
   belt4.scale = 1.5;
 
-  belt5 = createSprite(1350, 700);
+  belt5 = createSprite(width - 570, height - 269);
   belt5.addImage("beltImg", beltImg);
   belt5.scale = 1.5;
 
-  bin1 = createSprite(1700, 100, 210, 150);
+  bin1 = createSprite(width - 220, height - 869, 210, 150);
   bin1.addImage("binImg", binImg1);
   bin1.scale = 0.8;
   bin1.setCollider("rectangle", -50, 0, 300, 200);
 
-  bin2 = createSprite(1700, 250, 210, 150);
+  bin2 = createSprite(width - 220, height - 719, 210, 150);
   bin2.addImage("binImg", binImg2);
   bin2.scale = 0.8;
   bin2.setCollider("rectangle", -50, 0, 300, 200);
 
-  bin3 = createSprite(1700, 400, 210, 150);
+  bin3 = createSprite(width - 220, height - 569, 210, 150);
   bin3.addImage("binImg", binImg3);
   bin3.scale = 0.8;
   bin3.setCollider("rectangle", -50, 0, 300, 200);
 
-  bin4 = createSprite(1700, 550, 210, 150);
+  bin4 = createSprite(width - 220, height - 419, 210, 150);
   bin4.addImage("binImg", binImg4);
   bin4.scale = 0.8;
   bin4.setCollider("rectangle", -50, 0, 300, 200);
 
-  bin5 = createSprite(1700, 700, 210, 150);
+  bin5 = createSprite(width - 220, height - 269, 210, 150);
   bin5.addImage("binImg", binImg5);
   bin5.scale = 0.8;
   bin5.setCollider("rectangle", -50, 0, 300, 200);
 
-  openlid1 = createSprite(1700, 80);
+  openlid1 = createSprite(width - 220, 80);
   openlid1.visible = false;
   openlid1.addImage("openLid", openlid1Img);
 
-  openlid2 = createSprite(1700, 230);
+  openlid2 = createSprite(width - 220, height - 739);
   openlid2.visible = false;
   openlid2.addImage("openLid", openlid2Img);
 
-  openlid3 = createSprite(1700, 380);
+  openlid3 = createSprite(width - 220, height - 589);
   openlid3.visible = false;
   openlid3.addImage("openLid", openlid3Img);
 
-  openlid4 = createSprite(1700, 530);
+  openlid4 = createSprite(width - 220, height - 439);
   openlid4.visible = false;
   openlid4.addImage("openLid", openlid4Img);
 
-  openlid5 = createSprite(1700, 680);
+  openlid5 = createSprite(width - 220, height - 289);
   openlid5.visible = false;
   openlid5.addImage("openLid", openlid5Img);
 
-  player = createSprite(1000, 400);
+  player = createSprite(width - 920, height - 569);
   player.visible = false;
   player.scale = 0.5;
 }
@@ -162,41 +162,41 @@ function draw() {
     background(bg1);
     noStroke();
     stroke(2);
-    level = text("Level 1", 500, 50);
+    level = text("Level 1", width - 1420, height - 919);
   }
 
   if(points <= 20 && points >= 10) {
     noStroke();
     stroke(2);
     background(bg2);
-    level = text("Level 2", 500, 50);
+    level = text("Level 2", width - 1420, height - 919);
   }
 
   if(points <= 30 && points > 20) {
     noStroke();
     stroke(2);
     background(bg3);
-    level = text("Level 3", 500, 50);
+    level = text("Level 3", width - 1420, height - 919);
   }
 
   if(points <= 40 && points > 30) {
     noStroke();
     stroke(2);
     background(bg4);
-    level = text("Level 4", 500, 50);
+    level = text("Level 4", width - 1420, height - 919);
   }
 
   if(points <= 50 && points > 40) {
     noStroke();
     stroke(2);
     background(bg5);
-    level = text("Level 5", 500, 50);
+    level = text("Level 5", width - 1420, height - 919);
   }
 
   if(points > 50) {
     background(messageImg);
     fill("blue");
-    level = text("Good Job!", 100, 100);
+    level = text("Good Job!", width - 1820, height - 869);
     gameState = "Over";
     waste.velocityX = 0;
   }
@@ -207,24 +207,24 @@ function draw() {
     gameState = "Over";
     textSize(40);
     fill("red");
-    text("Sorry! Try again!", 100, 100);
+    text("Sorry! Try again!", width - 1820, height - 869);
     waste.velocityX = 0;
   }
 
   if(gameState === "Start") {
 
     //condition for player to appear
-    if(mouseX <= 1000 && mouseX >= 900 && mouseY >= 100 && mouseY <= 700) {
+    if((mouseX <= width - 920 && mouseX >= width - 1020 && mouseY >= height - 869 && mouseY <= height - 269) || (touches.length <= width - 920 && touches.length >= width - 1020)) {
       player.x = mouseX;
       player.y = mouseY;
       player.visible = true;
     }
  
     //condition for player to turn left and right
-    if(mouseX < 900 && mouseX > 800) {
+    if((mouseX < width - 1020 && mouseX > width - 1120) || (touches.length < width - 1020 && touches.length > width - 1120)) {
       player.addImage(playerImg);
       player.visible = true;
-    } else if(mouseX < 1000 && mouseX > 900){
+    } else if((mouseX < width - 920 && mouseX > width - 1020) || (touches.length < width - 920 && touches.length > width - 1020)){
       player.addImage(playerImg1);
       player.visible = true;
     }
@@ -232,7 +232,7 @@ function draw() {
     //conditions for assigning random images to the waste sprite and moving it
     if(frameCount === 100 ||wasteDestroy() === true) {     
 
-      waste = createSprite(300, 400);
+      waste = createSprite(width - 1620, height - 569);
       waste.scale = 0.8;
       waste.velocityX = 6 + 3*points/5;
     
@@ -282,60 +282,60 @@ function draw() {
   }
 
   //score conditions
-  if(anim === "bioImg" && (waste.x >= 1695 && waste.isTouching(bin1))) {
+  if(anim === "bioImg" && (waste.x >= width + 45 && waste.isTouching(bin1))) {
     points++;
 
-  } else if((waste.x >= 1695 && waste.isTouching(bin1)) && anim !== "bioImg"){    
+  } else if((waste.x >= width + 45 && waste.isTouching(bin1)) && anim !== "bioImg"){    
     points = points - 1;
   }
 
-  if(anim === "glassImg" && (waste.x >= 1695 && waste.isTouching(bin2))) {  
+  if(anim === "glassImg" && (waste.x >= width + 45 && waste.isTouching(bin2))) {  
     points++;
 
-  } else if((waste.x >= 1695 && waste.isTouching(bin2)) && anim !== "glassImg"){    
+  } else if((waste.x >= width + 45 && waste.isTouching(bin2)) && anim !== "glassImg"){    
     points = points - 1;
   }
 
-  if(anim === "metalImg" && (waste.x >= 1695 && waste.isTouching(bin3))) {   
+  if(anim === "metalImg" && (waste.x >= width + 45 && waste.isTouching(bin3))) {   
     points++;
     
-  } else if((waste.x >= 1695 && waste.isTouching(bin3)) && anim !== "metalImg"){     
+  } else if((waste.x >= width + 45 && waste.isTouching(bin3)) && anim !== "metalImg"){     
     points = points - 1;
   }
 
-  if(anim === "paperImg" && (waste.x >= 1695 && waste.isTouching(bin4))) {   
+  if(anim === "paperImg" && (waste.x >= width + 45 && waste.isTouching(bin4))) {   
     points++;
 
-  } else if((waste.x >= 1695 && waste.isTouching(bin4)) && anim !== "paperImg"){    
+  } else if((waste.x >= width + 45 && waste.isTouching(bin4)) && anim !== "paperImg"){    
     points = points - 1;
   }
 
-  if(anim === "plasticImg" && (waste.x >= 1695 && waste.isTouching(bin5))) {    
+  if(anim === "plasticImg" && (waste.x >= width + 45 && waste.isTouching(bin5))) {    
     points++;
 
-  } else if((waste.x >= 1695 && waste.isTouching(bin5)) && anim !== "plasticImg"){    
+  } else if((waste.x >= width + 45 && waste.isTouching(bin5)) && anim !== "plasticImg"){    
     points = points - 1;
   }
 
   //conditions to make SURE the belt touches the dustbin
-  if(waste.isTouching(belt1) && waste.y > -100 && waste.y <= 210) {
-    waste.y = 100;
+  if(waste.isTouching(belt1) && waste.y > height + 100 && waste.y <= height - 759) {
+    waste.y = height - 869;
   }
 
-  if(waste.isTouching(belt2) && waste.y > 210 && waste.y <= 360) {
-    waste.y = 250;
+  if(waste.isTouching(belt2) && waste.y > height - 759 && waste.y <= height - 609) {
+    waste.y = height - 719;
   }
 
-  if(waste.isTouching(beltExt3) && waste.y > 360 && waste.y <= 510) {
-    waste.y = 400;
+  if(waste.isTouching(beltExt3) && waste.y > height - 609 && waste.y <= height - 459) {
+    waste.y = height - 569;
   }
 
-  if(waste.isTouching(belt4) && waste.y > 510 && waste.y <= 660) {
-    waste.y = 550;
+  if(waste.isTouching(belt4) && waste.y > height - 459 && waste.y <= height - 309) {
+    waste.y = height - 429;
   }
 
-  if(waste.y > 660) {
-    waste.y = 700;
+  if(waste.y > height - 309) {
+    waste.y = height - 269;
   }
 
   drawSprites();
@@ -343,13 +343,13 @@ function draw() {
   //score display
   textSize(40);
   fill("white");
-  text("Score:" + " " + points, 100, 50);
+  text("Score:" + " " + points, width - 1820, height - 919);
 }
 }
 
 //to carry the waste with the mouse till a limit
 function touchMoved() {
-  if(waste.x < 1200) {
+  if(waste.x < width - 720) {
     waste.x = mouseX;
     waste.y = mouseY;
   }
@@ -357,10 +357,10 @@ function touchMoved() {
 
 //destroys the waste
 function wasteDestroy() {
-  if(waste.x > 1700) {
+  if(waste.x > width - 220) {
     waste.visible = false;
   }
-  if(waste.x >= 1725) {
+  if(waste.x >= width - 195) {
     waste.destroy();
     return true;
   } else {
